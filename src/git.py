254df4708fd9fe309541c2d256da7ac5591c5b6c
JoelHernandez343 <joel.programador@gmail.com>
2023-08-31 02:06:58 -0600

    Added automatic git bundle range calc

--- src/git.py
+++ src/git.py
@@ -49,7 +49,7 @@ def are_uncommited_changes() -> bool:
     return len(gitex(["status", "--porcelain"]).out.split("\n")) > 1
 
 
-def get_tags_w_commits(remote: str, branch: str) -> tuple[tuple[str]]:
+def get_tags_w_commits(remote: str, branch: str) -> tuple[tuple[str, str]]:
     tags = gitex(
         [
             "for-each-ref",
@@ -63,12 +63,31 @@ def get_tags_w_commits(remote: str, branch: str) -> tuple[tuple[str]]:
     return tuple(tuple(tag.replace("refs/tags/", "").split("-")) for tag in tags if tag)
 
 
-def current_branch() -> str | None:
+def get_current_branch() -> str | None:
     if branch := gitex(["branch", "--show-current"]).out.replace("\n", ""):
         return branch
     else:
         return None
 
 
-def current_commit() -> str:
+def get_current_commit() -> str:
     return gitex(["rev-parse", "HEAD"]).out.replace("\n", "")
+
+
+def get_branch_bundle_range(remote: str, branch: str) -> str:
+    current_commit = get_current_commit()
+    tags_w_commits = get_tags_w_commits(remote, branch)[:2]
+    number_of_tags = len(tags_w_commits)
+
+    if number_of_tags == 0:
+        return branch
+
+    last_commit = tags_w_commits[0][1]
+
+    if last_commit != current_commit:
+        return f"{last_commit}..{branch}"
+
+    if number_of_tags == 1:
+        return branch
+
+    return f"{tags_w_commits[1][1]}..{branch}"
