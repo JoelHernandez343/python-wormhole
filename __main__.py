5ec365983efe86caff887c3bdf4e6fa7a8792275
JoelHernandez343 <joel.programador@gmail.com>
2023-09-04 00:29:38 -0600

    Added header doc in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,3 +1,25 @@
+# Copyright 2023 Joel Harim Hernández Javier under MIT License
+# Github profile: https://github.com/JoelHernandez343
+#        _________________
+#       /                 \
+#      /      .  -  .      \
+#     /      { \   / }      \
+#    /        '-___-'        \
+#   /_________________________\
+#        _______: :_______
+#       /       | |       \
+#      /       /   \       \
+#     /      :'     ':      \
+#    /        '-----'        \
+#   /______ wormhole.git _____\
+#
+# Wormhole: Wormhole: Git bundler helper.
+# Allows syncing repos when direct acces is not available
+# using email
+# Based on http://stackoverflow.com/a/3639182/2877364
+# Art based in Henry Segerman's
+
+
 from .src.cli import init_cli
 from .src import git
 
